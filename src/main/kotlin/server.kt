e235985984cbb4cba5c808fb005520eb082fe43f
Christoph Flick <ChFlick@users.noreply.github.com>
2020-10-04 12:13:30 +0200

    Add basic authentication/security

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -4,12 +4,15 @@ import com.mongodb.MongoCredential
 import com.mongodb.WriteConcern
 import com.typesafe.config.ConfigFactory
 import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.config.*
 import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.routing.*
 import io.ktor.serialization.*
 import io.ktor.util.*
+import kotlinx.serialization.ExperimentalSerializationApi
 import org.kodein.di.bind
 import org.kodein.di.instance
 import org.kodein.di.ktor.di
@@ -18,6 +21,7 @@ import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 import routes.routes
+import security.JwtConfig
 import service.FormService
 import service.UserService
 import kotlin.text.toCharArray
@@ -45,12 +49,32 @@ fun initDatabase(): CoroutineDatabase {
     return client.getDatabase(config.property("mongodb.database").getString()) //normal java driver usage
 }
 
+@ExperimentalSerializationApi
 @KtorExperimentalAPI
 @SuppressWarnings("unused") // used in application.conf
 fun Application.main() {
+    di {
+        bind<CoroutineDatabase>() with singleton { initDatabase() }
+        bind<UserService>() with singleton { UserService(instance()) }
+        bind<FormService>() with singleton { FormService(instance()) }
+    }
+    
+    val userService by di().instance<UserService>()
+
+    install(Authentication) {
+        jwt {
+            verifier(JwtConfig.verifier)
+            realm = "postithere"
+            validate {
+                it.payload.getClaim("id").asString()?.let { id -> userService.getUserById(id) }
+            }
+        }
+    }
+
     install(ContentNegotiation) {
         json()
     }
+
     install(CORS) {
         method(HttpMethod.Options)
         method(HttpMethod.Get)
@@ -61,18 +85,15 @@ fun Application.main() {
         header(HttpHeaders.AccessControlAllowHeaders)
         header(HttpHeaders.ContentType)
         header(HttpHeaders.AccessControlAllowOrigin)
+        header(HttpHeaders.Authorization)
         allowCredentials = true
         anyHost()
         maxAgeInSeconds = 86400L
     }
+
     install(DefaultHeaders)
     install(CallLogging)
     install(Routing) {
         routes()
-    }
-    di {
-        bind<CoroutineDatabase>() with singleton { initDatabase() }
-        bind<UserService>() with singleton { UserService(instance()) }
-        bind<FormService>() with singleton { FormService(instance()) }
     }
 }
--- src/main/kotlin/service/UserService.kt
+++ src/main/kotlin/service/UserService.kt
@@ -1,10 +1,16 @@
 package service
 
 import com.mongodb.WriteConcern
-import kotlinx.serialization.Serializable
+import io.ktor.auth.*
+import kotlinx.html.InputType
+import kotlinx.serialization.*
+import org.bson.types.ObjectId
+import org.litote.kmongo.Id
 import org.litote.kmongo.addToSet
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.eq
+import org.litote.kmongo.id.MongoId
+import org.mindrot.jbcrypt.BCrypt
 
 @Serializable
 data class ApiKey(
@@ -15,25 +21,49 @@ data class ApiKey(
 
 @Serializable
 data class User(
+    @Contextual @SerialName("_id") @MongoId val id: Id<User>? = null,
     val email: String,
     val password: String,
-    val apiKey: ApiKey,
-    val formIds: List<String>
-)
+    val apiKey: ApiKey? = null,
+    val formIds: List<String>? = emptyList()
+) : Principal
+
+@Serializable
+data class EmailPasswordCredential (val email: String, val password: String) : Credential
 
 class UserService(database: CoroutineDatabase) {
-    private val userCollection = database.getCollection<User>().withWriteConcern(WriteConcern.MAJORITY)
+    private val userCollection = database.getCollection<User>("users").withWriteConcern(WriteConcern.MAJORITY)
 
     suspend fun getUserByEmail(email: String): User? {
         return userCollection.findOne(User::email eq email)
     }
 
     suspend fun addUser(user: User): Boolean {
-        return userCollection.insertOne(user).wasAcknowledged()
+        val userWithHashedPassword = User(
+            user.id,
+            user.email,
+            BCrypt.hashpw(user.password, BCrypt.gensalt()),
+            user.apiKey,
+            user.formIds
+        )
+
+        return userCollection.insertOne(userWithHashedPassword).wasAcknowledged()
     }
 
     suspend fun addFormToUser(user: User, formId: String): Boolean {
-        return userCollection.updateOne(User::email eq user.email, addToSet(User::formIds, formId)).modifiedCount == 1L
+        return userCollection.updateOne(
+            User::email eq user.email,
+            addToSet(User::formIds, formId)
+        ).modifiedCount == 1L
+    }
+
+    suspend fun getUserById(userId: String): User? {
+        return userCollection.findOneById(ObjectId(userId))
+    }
+
+    suspend fun getUserByCredentials(credential: EmailPasswordCredential): User? {
+        val user = userCollection.findOne(User::email eq credential.email) ?: return null
+        return if (BCrypt.checkpw(credential.password, user.password)) user else null
     }
 
 }
