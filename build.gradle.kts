78f81c4e61054741805f185decaae6bf773bdad8
Christoph Flick <ChFlick@users.noreply.github.com>
2020-10-19 08:27:18 +0200

    Add git-tag-based versioning system

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,26 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ktor_version: String by project
-val kodein_version: String by project
-val logback_version: String by project
-val kotest_version: String by project
-val mockk_version: String by project
+val ktorVersion: String by project
+val kodeinVersion: String by project
+val logbackVersion: String by project
+val kotestVersion: String by project
+val mockkVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.4.10"
     kotlin("plugin.serialization") version "1.4.10"
     id("com.adarshr.test-logger") version "2.1.0"
-    application
+    id("pl.allegro.tech.build.axion-release") version "1.12.1"
     jacoco
+    application
+}
+
+scmVersion {
+    tag.prefix = ""
 }
 
 group = "org.postithere"
-version = "1.0-SNAPSHOT"
+version = scmVersion.version
 
 repositories {
     mavenCentral()
@@ -23,29 +28,29 @@ repositories {
 }
 
 dependencies {
-    testImplementation("io.kotest:kotest-runner-junit5:$kotest_version") // for kotest framework
-    testImplementation("io.kotest:kotest-assertions-core:$kotest_version") // for kotest core jvm assertions
-    testImplementation("io.kotest:kotest-property:$kotest_version") // for kotest property test
-    testImplementation("io.kotest:kotest-assertions-ktor:$kotest_version") // for kotest property test
-    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
-    testImplementation("io.mockk:mockk:$mockk_version")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion") // for kotest framework
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion") // for kotest core jvm assertions
+    testImplementation("io.kotest:kotest-property:$kotestVersion") // for kotest property test
+    testImplementation("io.kotest:kotest-assertions-ktor:$kotestVersion") // for kotest property test
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
     testImplementation("org.testcontainers:mongodb:1.14.3")
 
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("io.ktor:ktor-html-builder:$ktor_version")
-    implementation("io.ktor:ktor-serialization:$ktor_version")
-    implementation("io.ktor:ktor-auth:$ktor_version")
-    implementation("io.ktor:ktor-auth-jwt:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-html-builder:$ktorVersion")
+    implementation("io.ktor:ktor-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-auth:$ktorVersion")
+    implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
     implementation("org.litote.kmongo:kmongo-coroutine-serialization:4.1.2")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("org.kodein.di:kodein-di:$kodein_version")
-    implementation("org.kodein.di:kodein-di-framework-ktor-server-jvm:$kodein_version")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("org.kodein.di:kodein-di:$kodeinVersion")
+    implementation("org.kodein.di:kodein-di-framework-ktor-server-jvm:$kodeinVersion")
     implementation("org.mindrot:jbcrypt:0.4")
     implementation("com.configcat:configcat-android-client:5.+")
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 tasks.withType<Test> {
