45b0e44266d77e6bd3a27159e9d87920f762fd1e
Christoph Flick <ChFlick@users.noreply.github.com>
2020-10-26 08:15:26 +0100

    Setup workspace

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,29 +1,37 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
+  extends: [
     'plugin:react/recommended',
-    'google',
+    'airbnb-typescript',
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  'plugins': [
+  plugins: [
     'react',
     '@typescript-eslint',
   ],
-  'rules': {
+  globals: {
+    React: 'writable',
+    next: 'writable',
+  },
+  rules: {
     'react/react-in-jsx-scope': 'off',
     'require-jsdoc': 'off',
-  },
-  'globals': {
-    'React': 'writable',
+    'max-len': ['error', { code: 120, tabWidth: 2, comments: 120 }],
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'import/no-extraneous-dependencies': 'off',
+    // 'react/jsx-max-props-per-line': ['error', { maximum: 2 }],
   },
 };
